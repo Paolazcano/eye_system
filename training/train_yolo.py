746e42425ddf376fb955bec811c0ec194a85a831
Paolazcano <157818042+Paolazcano@users.noreply.github.com>
2026-02-09 22:53:51 -0600

    Confirmar

--- training/train_yolo.py
+++ training/train_yolo.py
@@ -6,12 +6,12 @@ def main():
 
     model.train(
         data="dataset/data.yaml",
-        epochs=100,
-        imgsz=640,
-        batch=8,
-        optimizer="AdamW",
-        lr0=0.001,
-        workers=2,
+        epochs=100,          # Epocas: Número de veces que el modelo ve todo el dataset (100 es el de ley)
+        imgsz=640,           # Tamaño al que se redimensionan las imágenes (Se puede intentar con 768)
+        batch=8,             # Número de imágenes procesadas al mismo tiempo (Depende del GPU)
+        optimizer="AdamW",   # Optimizador usado para ajustar los pesos
+        lr0=0.001,           # Learning rate inicial (Valor estándar para AdamW)
+        workers=2,           # Número de "trabajadores"para cargar datos
         project="models/yolo",
         name="eye_detector"
     )
